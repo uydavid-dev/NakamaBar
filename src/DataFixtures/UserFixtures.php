252b1507967560d161658df53ca6b2def8734a6e
uydavid-dev <david.heng@hotmail.fr>
2021-10-27 16:11:22 +0200

    premier repository
    creation du projet sur symfony
    creation d'un user
    creation base navbar et footer
    installation webpack encore
    premiere migration
    installation de fixture
    creation Auth donc page login
    creation de template index/lacarte/reservation/welcome

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
+    public function load(ObjectManager $manager): void
+    {
+
+        $user = new User();
+
+        $user   ->setEmail('user@user.fr')
+                ->setPrenom('user')
+                ->setNom('test')
+                ->setTelephone('0800801801');
+
+        $password = $this->encoder->encodePassword($user, 'password');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
